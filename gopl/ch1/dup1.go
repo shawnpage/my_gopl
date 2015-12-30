ea22fe70993542ae52600b61facb50d18cd0fddd
Shawn Page <shawn.page@gmail.com>
2015-12-30 18:01:36 -0500

    Use ioutil.ReadFile() to read file contents
    Also, move files into ch1 subdir

---
+++ gopl/ch1/dup1.go
@@ -0,0 +1,23 @@
+// Dup1 prints the text f each line that appears more tha
+// once in the standard input, preceded buy its count.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%d\n", line, n)
+		}
+	}
+}
