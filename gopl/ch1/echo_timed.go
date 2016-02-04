7e54e3e7043486866e541265d28a85f514a62ce8
Shawn Page <shawn.page@gmail.com>
2016-02-04 09:50:20 -0500

    Add fetch.go for ch1.5 Some formatting included

--- gopl/ch1/echo_timed.go
+++ gopl/ch1/echo_timed.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"testing"
 	"time"
 )
 
--- gopl/ch1/lissajous.go
+++ gopl/ch1/lissajous.go
@@ -47,7 +47,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-//			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			//			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex+uint8(index%3))
 			index++
 		}
